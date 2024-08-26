6dc1e7293e327ecce0224f77b9adcad47b34f2d7
Giovanni Francesco Guarnieri <51720601+gfguarnieri@users.noreply.github.com>
2024-08-26 11:24:13 +0000

    App de Nome Completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { AlertController, IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public nome: string = "";
+  public sobrenome: string = "";
+  public nomeCompleto: string = "";
+
+  constructor(private alertController:AlertController) { }
+
+  async aviso() {
+
+    const mensagem = await this.alertController.create({
+      header: 'Nome completo',
+      message: 'O seu nome completo é ' + this.nomeCompleto,
+      buttons: ["OK"]
+    });
+
+    await mensagem.present();
+  }
+
+  concatenarNomes(){
+    this.nomeCompleto = this.nome + " " + this.sobrenome;
+    this.aviso();
+  }
+  
 }
